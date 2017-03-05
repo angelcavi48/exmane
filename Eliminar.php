c6a16e0ad99a76cfed5ae714c0c24715f12e403d
angelcavi48 <angelcavi48@gmail.com>
2017-03-05 00:38:59 -0600

    f

--- Eliminar.php
+++ Eliminar.php
@@ -1,14 +1,14 @@
 <?php
 
 include "conexion.php";
-echo "entro"
-$Idser=$_POST['Id'];
+echo "entro";
+$Idservi=$_POST['Ids'];
 
-$sql="DELETE FROM servicios where id_Servicios =$Idser";
+$sql="DELETE FROM servicios where id_Servicios=$Idservi";
 $resp=mysqli_query($conn,$sql);
-echo "borro"
+echo "borro";
 if($resp){
-echo "el registro fue eliminado correctamente"
+echo "el registro fue eliminado correctamente";
 	}
 }else{
 	echo "Ocurrio un error al eliminar el registro".mysqli_error();
