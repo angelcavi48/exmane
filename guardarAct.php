b2f5e038029ce40a8b7cff1eb932c34767382b2d
angelcavi48 <angelcavi48@gmail.com>
2017-03-11 18:20:03 -0600

    k

--- guardarAct.php
+++ guardarAct.php
@@ -1,14 +1,16 @@
 <?php
 //Se Agrega el archivo que contine la conexion a la BD
 include "conexion.php";
-$id=$_POST['id'];
-$nom=$_POST['nombre'];
-$dir=$_POST['dir'];
-$tel=$_POST['tel'];
-$rfc=$_POST['rfc'];
-$lat=$_POST['lat'];
-$longt=$_POST['long'];
+$id=$_POST['Id'];
+$nom=$_POST['Nom'];
+$dir=$_POST['Dir'];
+$rfc=$_POST['Rfc'];
+$tel=$_POST['Tel'];
+$clasif=$_POST['Clasif'];
 $foto="images/".$tel.".jpg";
+$lat=$_POST['Lat'];
+$longt=$_POST['Long'];
+
 
 $sql="UPDATE servicios SET Nombre_servicio='$nom', Direccion='$dir', RFC='$rfc',Telefono='$tel',Latitud='$lat', Longitud='$longt' WHERE id=$id";
 $res=mysqli_query($conexion,$sql);
